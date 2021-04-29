5464b1a5edb1ad177bfc70886920ea4259f41c4b
Bereket Godebo <15813909+bereketgodebo@users.noreply.github.com>
2021-04-29 11:17:53 +0300

    Finished the practice

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import  DaiToken from '../abis/DaiToken.json'
 import DappToken from '../abis/DappToken.json'
 import TokenFarm from '../abis/TokenFarm.json'
 import Navbar from './Navbar'
+import Main from './Main'
 import './App.css'
 
 class App extends Component {
@@ -80,6 +81,31 @@ class App extends Component {
 
   }
 
+  stakeTokens = (amount) => {
+      this.setState({ loading: true})
+      this.state.daiToken.methods.approve(
+            this.state.tokenFarm._address, amount
+      ).send({ from: this.state.account })
+       .on('transactionHash', (hash) => {
+              this.state.tokenFarm.methods.stakeTokens(amount)
+              .send({ from: this.state.account})
+              .on('transactionHash', (hash) => {
+                this.setState({loading: false})
+        })
+      })
+  }
+
+  unstakeTokens = (amount) => {
+    this.setState({ loading: true})
+    this.state.tokenFarm.methods.unstakeTokens()
+          .send({ from: this.state.account })
+          .on('transactionHash', (hash) => {
+            this.setState( { loading: false})
+          })
+  }
+
+
+
   constructor(props) {
     super(props)
     this.state = {
@@ -95,6 +121,18 @@ class App extends Component {
   }
 
   render() {
+    let content
+    if(this.state.loading) {
+      content = <p id="loader" className="text-center">Loading...</p>
+    } else {
+      content = <Main
+                  daiTokenBalance = {this.state.daiTokenBalance}
+                  dappTokenBalance = {this.state.dappTokenBalance}
+                  stakingBalance = {this.state.stakingBalance}
+                  stakeTokens = {this.stakeTokens}
+                  unstakeTokens = {this.unstakeTokens}
+              />
+    }
     return (
       <div>
         <Navbar account={this.state.account} />
@@ -108,9 +146,7 @@ class App extends Component {
                   rel="noopener noreferrer"
                 >
                 </a>
-
-                <h1>Hello, World!</h1>
-
+                  {content}
               </div>
             </main>
           </div>
